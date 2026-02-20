4bc3f6c17fdebf1f36a3b34820ebdf44560d66f8
vijay kumar <kumarvj448@gmail.com>
2026-02-20 15:57:30 +0530

    save

--- src/main/java/pageObjects/ContactPage1.java
+++ src/main/java/pageObjects/ContactPage1.java
@@ -3,7 +3,6 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.testng.annotations.Test;
 
 public class ContactPage1 extends BasePage1 {
 
